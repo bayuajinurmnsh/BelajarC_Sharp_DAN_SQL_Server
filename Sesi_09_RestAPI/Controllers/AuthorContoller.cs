b531d5fd334eae4bf9ed342d24906ee1c24e6f16
bayuajinurmnsh <bayuaji.nurmansah16@gmail.com>
2021-10-15 11:41:47 +0700

    push sesi 9 and add hasil ss and hasil json

---
+++ Sesi_09_RestAPI/Controllers/AuthorContoller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Sesi_09_RestAPI.Models;
+
+namespace Sesi_09_RestAPI.Controllers
+{
+    [Route("api/author")]
+    [ApiController]
+    public class AuthorContoller : ControllerBase
+    {
+        private EmployeeContext _context;
+        public AuthorContoller(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/User
+        public ActionResult<IEnumerable<AuthorItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            return _context.GetAllAuthor();
+        }
+    }
+}
---
+++ Sesi_09_RestAPI/Models/EmployeeItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sesi_09_RestAPI.Models
+{
+    public class EmployeeItem
+    {
+        private Models.EmployeeContext context;
+
+        public int id { get; set; }
+        public string nama { get; set; }
+        public string jenisKelamin { get; set; }
+        public string alamat { get; set; }
+    }
+
+    public class AuthorItem
+    {
+        private Models.EmployeeContext context;
+        public int id { get; set; }
+        public string author_name { get; set; }
+    }
+}
